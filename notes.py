5a75edede9130dfb4a3c9de7903423f25bef9bba
Jesse <jesse@Jesses-MacBook-Pro.local>
2020-02-19 13:33:33 +1100

    commit from work Macbook

---
+++ notes.py
@@ -0,0 +1,7 @@
+#Dictionaries
+
+.pop() # removes key/value pair
+.keys() # retrieves list of keys
+.items() # retrieves key/value pairs
+    for key, value in dict.items():
+        print("Input key:" + key + "and Value: " + value)
