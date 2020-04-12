8f917288e82eca0f9af75194a4d6e5ffe9889f8c
jessewindebank1809 <60115932+jessewindebank1809@users.noreply.github.com>
2020-04-12 16:01:02 +1000

    changes

--- Intro to React/number-guesser-starting/script.js
+++ Intro to React/number-guesser-starting/script.js
@@ -3,15 +3,15 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 function generateTarget(max) {
-    return Math.floor(Math.random * max)
+    return Math.floor(Math.random() * max)
 }
 
 function compareGuesses(human, computer, target) {
     let humanGuess = target - human;
-    console.log(`human difference is ${humanGuess}`);
+    console.log(`Human difference is ${humanGuess}`);
     let computerGuess = target - computer;
-    console.log(`computer difference is ${computerGuess}`);
-    if (Math.abs(humanGuess <= computerGuess)) {
+    console.log(`Computer difference is ${computerGuess}`);
+    if (humanGuess <= computerGuess) {
         return true
     } else {
         return false
@@ -21,22 +21,24 @@ function compareGuesses(human, computer, target) {
 function updateScore(winner) {
     if (winner === true) {
         console.log('Human WINS!');
-        return humanScore +=
+        return humanScore + 1;
     } else {
         console.log('Computer WINS!');
-        return computerScore +=
+        return computerScore + 1;
     }
 }
 
 function advanceRound() {
-    currentRoundNumber +=
-    console.log(`Computer score is ${computerScore}`);
-    console.log(`Human score is ${computerScore}`);
-    console.log(`Current round number is ${currentRoundNumber}`);
+    currentRoundNumber = currentRoundNumber + 1;
+    return currentRoundNumber
 }
 
-const target = generateTarget(9);
+const target = generateTarget(10);
 console.log(`The target is ${target}`);
-const compare = compareGuesses(3, 5, generateTarget);
+const compare = compareGuesses(3, 5, target);
 const update = updateScore(compare);
 const updateRound = advanceRound();
+
+console.log(`Computer score is ${computerScore}`);
+console.log(`Human score is ${humanScore}`);
+console.log(`Current round number is ${currentRoundNumber}`);
