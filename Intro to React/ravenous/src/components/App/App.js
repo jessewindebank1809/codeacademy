b16948e6975c75cff1f2964b95c953c71ae29675
jessewindebank1809 <60115932+jessewindebank1809@users.noreply.github.com>
2020-04-20 09:44:40 +1000

    further updates

--- Intro to React/ravenous/src/components/App/App.js
+++ Intro to React/ravenous/src/components/App/App.js
@@ -4,13 +4,52 @@ import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
+class Business extends React.Component {
+  render() {
+    return (
+      <div className="Business">
+        <div className="image-container">
+          <img
+            src="https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg"
+            alt=""
+          />
+        </div>
+        <h2>{Business.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{Business.address}</p>
+            <p>{Business.city}</p>
+            <p>
+              {Business.state} {Business.zipCode}
+            </p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{Business.category}</h3>
+            <h3 className="rating">{Business.rating} stars}</h3>
+            <p>{Business.reviewCount} reviews</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const businesses = [
+  { Business },
+  { Business },
+  { Business },
+  { Business },
+  { Business },
+  { Business }
+];
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
--- Intro to React/ravenous/src/components/Business/Business.js
+++ Intro to React/ravenous/src/components/Business/Business.js
@@ -14,34 +14,4 @@ const business = {
   reviewCount: 90
 };
 
-class Business extends React.Component {
-  render() {
-    return (
-      <div className="Business">
-        <div className="image-container">
-          <img
-            src="https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg"
-            alt=""
-          />
-        </div>
-        <h2>{Business.name}</h2>
-        <div className="Business-information">
-          <div className="Business-address">
-            <p>{Business.address}</p>
-            <p>{Business.city}</p>
-            <p>
-              {Business.state} {Business.zipCode}
-            </p>
-          </div>
-          <div className="Business-reviews">
-            <h3>{Business.category}</h3>
-            <h3 className="rating">{Business.rating} stars}</h3>
-            <p>{Business.reviewCount} reviews</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
 export default Business;
