5d5462f781adc0da1e9fddb5c1f6f70c77142fa6
jessewindebank1809 <60115932+jessewindebank1809@users.noreply.github.com>
2020-04-20 08:31:33 +1000

    updated searchbar

--- Intro to React/ravenous/src/components/SearchBar/SearchBar.js
+++ Intro to React/ravenous/src/components/SearchBar/SearchBar.js
@@ -3,31 +3,33 @@ import "./SearchBar.css";
 
 const sortByOptions = {
   "Best Match": "best_match",
-  "Highest Rated": "highest_rated",
-  "Most Reviewed": "most_reviewed"
+  "Highest Rated": "rating",
+  "Most Reviewed": "review_count"
 };
 
 class SearchBar extends React.Component {
-  renderSortByOptions(sortByOptions) {
+  renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
-      const sortByOptionValue = sortByOptions[sortByOption];
-      <li key="sortByOptionValue">{sortByOption}</li>;
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
   }
 
   render() {
-    <div className="SearchBar">
-      <div className="SearchBar-sort-options">
-        <ul>renderSortByOptions()</ul>
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{this.renderSortByOptions()}</ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
       </div>
-      <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
-      </div>
-      <div className="SearchBar-submit">
-        <a>Let's Go</a>
-      </div>
-    </div>;
+    );
   }
 }
 
