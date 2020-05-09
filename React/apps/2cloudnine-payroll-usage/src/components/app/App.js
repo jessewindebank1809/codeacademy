bca76770441ca0d5d8a0e8e50c844e00b1525378
jesse.windebank@gmail.com <60115932+jessewindebank1809@users.noreply.github.com>
2020-05-09 15:52:56 +1000

    first updates

---
+++ React/apps/2cloudnine-payroll-usage/src/components/app/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>2cloudnine payroll</p>
+        <p>Month 1: 78</p>
+        <p>Month 2: 43</p>
+        <p>Month 3: 31</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/apps/2cloudnine-payroll-usage/src/components/salesforce/salesforce.js
@@ -0,0 +1,30 @@
+import React from "react";
+let accessToken;
+const username = "jessew@2cloudnine.com";
+const password = "hB1RA4mJIm41jX10VrYDCzgOUBNahvVNblkFZ";
+const consumerKey =
+  "3MVG9Y6d_Btp4xp6Qbj1B103tOLFeipX5Hx6L1eid5xunTxNcryr2Pkv2lPtyOPfbDQBfecKRm2ex8.wTsR9q";
+const consumerSecret =
+  "9FF4BFBBB6674A368B104E031217F8A317A5BE53CD8CF5C187D998B6FA4739CF";
+const headers = { "Content-Type": "application/x-www-form-urlencoded" };
+
+const Salesforce = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+    return fetch(`https://login.salesforce.com/services/oauth2/token`, {
+      headers: headers,
+      method: "POST",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        grant_type: "password",
+        client_id: consumerKey,
+        client_secret: consumerSecret
+      })
+    });
+  }
+};
+
+export default Salesforce;
